03997dd7f14a166c6960dde9c4e9290148e55389
Adrian Siekierka <kontakt@asie.pl>
2026-06-20 13:01:12 +0200

    nileswan: fix MCU USB CDC stub

--- Core/WS/Carts/Nileswan/NileMCU.cpp
+++ Core/WS/Carts/Nileswan/NileMCU.cpp
@@ -296,8 +296,8 @@ uint8_t NileMCU::SpiExchange(uint8_t tx) {
                         continue;
                     }
                     // TODO
-                    /* if (!Comm_RecvByte(response + len))
-                        break; */
+                    /* if (!Comm_RecvByte(response + len)) */
+                        break;
                 }
                 printf("nileswan/spi/mcu: USB serial read %d bytes, found %d\n", arg, len);
                 SendSpiResponse(len, response);
@@ -309,11 +309,10 @@ uint8_t NileMCU::SpiExchange(uint8_t tx) {
                 printf("nileswan/spi/mcu: USB serial write %d bytes\n", arg);
                 int len = 0;
                 // TODO
-                len = arg;
-                /* for (; len < arg; len++) {
-                    if (!Comm_SendByte(rxBuf.data[len]))
+                for (; len < arg; len++) {
+                    /* if (!Comm_SendByte(rxBuf.data[len])) */
                         break;
-                } */
+                }
                 rxBuf.Pop(NULL, arg);
                 SendSpiResponse(2, &len);
             } break;
